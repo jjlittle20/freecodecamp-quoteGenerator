fffb963290be909fb5f8a50f6b35123d155f6d53
James <jameslittle12@hotmail.com>
2023-03-04 13:36:11 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quoteData, setQuoteData] = useState({ text: "", author: "" });
+
+  const getQuoteData = async () => {
+    fetch("https://api.quotable.io/random")
+      .then(async (response) => {
+        const isJson = response.headers
+          .get("content-type")
+          ?.includes("application/json");
+
+        const data = isJson ? await response.json() : null;
+
+        if (!response.ok) {
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+
+        const newQuoteData = {
+          text: data?.content,
+          author: data?.author,
+        };
+
+        setQuoteData(newQuoteData);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  useEffect(() => {
+    getQuoteData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a href="" className="d-flex justify-content-end">
+        Github Repo
+      </a>
+      <div className=" d-flex justify-content-center align-items-center h-100 d-inline-block p-4">
+        <div className="card" id="quote-box">
+          <div class="card-body max-w-40">
+            <div id="text" style={{ wordWrap: "break-word" }}>
+              {quoteData?.text}
+            </div>
+            <div className="mt-4  d-flex justify-content-end" id="author">
+              -{quoteData?.author}
+            </div>
+            <div className="d-flex justify-content-between mt-4">
+              <button id="new-quote" onClick={() => getQuoteData()}>
+                New Quote
+              </button>
+              <a href="twitter.com/intent/tweet" id="tweet-quote">
+                Tweet
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
